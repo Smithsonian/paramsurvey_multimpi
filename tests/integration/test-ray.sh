bbe342a0f31ca40a41a68ae62b213bf953e00f89
Greg Lindahl <lindahl@pbm.com>
2022-04-20 17:46:56 +0000

    try CI

--- tests/integration/test-ray.sh
+++ tests/integration/test-ray.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-PORT=6379
+if [ ! -z "$ONLY_BUILTINS" ]; then
+    exit 0
+fi
+
+    PORT=6379
 REDIS_PASSWORD=thehfhghedhdjfhgfhdhdhdf
 echo $(hostname):$PORT $REDIS_PASSWORD > ~/.ray-test-72363726-details
 
@@ -10,8 +14,6 @@ GIGABYTE=1000000000  # close enough
 
 ray stop  # in case there's an a leftover ray
 ray start --head --port=$PORT --redis-password=$REDIS_PASSWORD --memory $GIGABYTE --object-store-memory $GIGABYTE --redis-max-memory $GIGABYTE $RAY_START_EXTRAS
-
-echo remaining args are "$@"
 
 RAY_HEAD_FILE=~/.ray-test-72363726-details PARAMSURVEY_BACKEND=ray pytest "$@"
 
