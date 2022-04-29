ea2f3839cb943d99f5166791821b486a0b2fbc73
Greg Lindahl <lindahl@pbm.com>
2022-04-29 17:54:51 +0000

    minimum number of ray cpus

--- tests/integration/test-ray.sh
+++ tests/integration/test-ray.sh
@@ -13,7 +13,7 @@ echo $(hostname):$PORT $REDIS_PASSWORD > ~/.ray-test-72363726-details
 GIGABYTE=1000000000  # close enough
 
 ray stop  # in case there's an a leftover ray
-ray start --head --port=$PORT --redis-password=$REDIS_PASSWORD --memory $GIGABYTE --object-store-memory $GIGABYTE --redis-max-memory $GIGABYTE $RAY_START_EXTRAS
+ray start --head --port=$PORT --redis-password=$REDIS_PASSWORD --memory $GIGABYTE --object-store-memory $GIGABYTE --redis-max-memory $GIGABYTE $RAY_START_EXTRAS --num-cpus 4
 
 RAY_HEAD_FILE=~/.ray-test-72363726-details PARAMSURVEY_BACKEND=ray pytest "$@"
 
